477f1cebac9455b1d2b33ecabce441dfd25446cf
Shynd <shynd@protonmail.com>
2020-05-15 23:36:31 +0200

    ...

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace NixSouls
     {
         private static ILogger _logger = LogManager.GetForClass();
 
+        // I bet this the last folder is system-specific and is gonna fuck me over, just change it if you need to for now...
         private const string GAME_PATH = "/.steam/steam/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/Application Data/DarkSoulsIII/01100001028087fa/";
         private const string SAVE_FILE = "DS30000.sl2";
 
